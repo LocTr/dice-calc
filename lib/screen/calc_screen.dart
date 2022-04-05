c2dc033ff120ed5228272ac37081ce2965e28e1a
Loc <ttloc1999@gmail.com>
2022-04-05 16:19:19 +0700

    add button grid

--- lib/screen/calc_screen.dart
+++ lib/screen/calc_screen.dart
@@ -8,22 +8,47 @@ class CalcScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: const [
-                Text(
-                  '31',
+        child: SizedBox.expand(
+          child: Container(
+            color: Colors.white70,
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  height: 200.0,
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        '64',
+                        style: TextStyle(
+                          fontSize: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    physics: const NeverScrollableScrollPhysics(),
+                    crossAxisSpacing: 1,
+                    mainAxisSpacing: 2,
+                    crossAxisCount: 4,
+                    // children: <Widget> [
+                    //   TextButton(onPressed: () {}, child: Text('1'))
+                    // ],
+                    children: List.generate(16, (index) {
+                      return TextButton(
+                        onPressed: () {},
+                        child: Text(index.toString()),
+                      );
+                    }),
+                  ),
                 )
               ],
             ),
-            MaterialButton(
-              onPressed: () {},
-              child: Text('asdfasdf'),
-            ),
-          ],
+          ),
         ),
       ),
     );
