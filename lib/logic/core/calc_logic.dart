7dcaeb12f0bdbbe0bf22f6d9fb95ed238c57e172
Loc <ttloc1999@gmail.com>
2022-05-09 15:53:03 +0700

    fix split regex and update test case

--- lib/logic/core/calc_logic.dart
+++ lib/logic/core/calc_logic.dart
@@ -1,11 +1,24 @@
 List<String> splitDice(String input) {
   List<String> result;
-  final regex = RegExp(r'\+|\-');
+  // ?= is lookahead regex
+  // this regex mean split by '+' or by the space before '-'
+  // hence it keep the minus sign if present.
+  final regex = RegExp(r'(\+)|(?=\-)');
   result = input.split(regex);
   return result;
 }
 
 List<String> addChar(String input) {
   List<String> result = [];
+  return result;
+}
+
+int rollDices(List<String> input) {
+  int result = 0;
+  //element is dices or bonuses
+  for (var element in input) {
+    //is a dice
+    if (element.contains('d')) {}
+  }
   return result;
 }
--- test/core_test.dart
+++ test/core_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Screen string should split into dices and bonuses', () {
-    const String input = '2d2+4d6+7';
+    const String input = '2d2+4d6-7';
     final result = splitDice(input);
-
     expect(result[0], '2d2');
     expect(result[1], '4d6');
+    expect(result[2], '-7');
   });
 }
