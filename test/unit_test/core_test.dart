9803e34f5da55cb28cd458218379be85bc64ee94
Loc <ttloc1999@gmail.com>
2022-05-19 16:10:33 +0700

    update test case

--- test/unit_test/core_test.dart
+++ test/unit_test/core_test.dart
@@ -1,28 +1,64 @@
 import 'package:dice_calc/logic/core/calc_logic.dart';
 import 'package:dice_calc/model/element.dart';
+import 'package:dice_calc/model/enums.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Roll dice function', () {
-    test('test dice roll result (1) | single empty d6', () {
-      const List<Element> input = [DiceElement(content: '')];
-      for (var i = 0; i < 1000; i++) {
-        var result = rollDices(input);
-        // minimum roll is 1, maximum roll is 11
-        expect(result, inInclusiveRange(1, 6));
-      }
-    });
+  group(
+    'Single dice roll with multiplier',
+    () {
+      test('test dice roll result (1) | single empty d6', () {
+        const List<Element> input = [DiceElement(content: '')];
+        for (var i = 0; i < 1000; i++) {
+          var result = rollDices(input);
+          // minimum roll is 1, maximum roll is 11
+          expect(result, inInclusiveRange(1, 6));
+        }
+      });
 
-    test('test dice roll result (2)  | 3d7', () {
+      test('test dice roll result (2)  | 3d7', () {
+        const List<Element> input = [
+          NumberElement(content: '3'),
+          DiceElement(content: '7'),
+        ];
+        for (var i = 0; i < 1000; i++) {
+          var result = rollDices(input);
+          // minimum roll is -9, maximum roll is 3
+          expect(result, inInclusiveRange(-9, 3));
+        }
+      });
+    },
+  );
+
+  group('Dice roll with multiple operator', () {
+    test('test dice roll with minus op', () {
       const List<Element> input = [
-        NumberElement(content: '3'),
-        DiceElement(content: '7'),
+        NumberElement(content: '0'),
+        OperatorElement(content: '-', operator: Operator.plus),
+        DiceElement(content: '6'),
       ];
       for (var i = 0; i < 1000; i++) {
         var result = rollDices(input);
-        // minimum roll is -9, maximum roll is 3
-        expect(result, inInclusiveRange(-9, 3));
+        //minimum roll is -6, maximum roll is 0
+        expect(result, inInclusiveRange(-6, 6));
       }
+    });
+
+    test('test dice roll with multiply op', () {
+      //this test case could fail at 1 in 7.06e-779 chances
+      const List<Element> input = [
+        NumberElement(content: '2'),
+        OperatorElement(content: 'x', operator: Operator.multiply),
+        DiceElement(content: ''),
+      ];
+      Set expectedResult = <int>{};
+      for (var i = 0; i < 1000; i++) {
+        int rollResult = rollDices(input);
+
+        expectedResult.add(rollResult);
+        //minimum roll is 12, maximum roll is 0
+      }
+      expect(expectedResult, containsAll([2, 4, 6, 8, 10, 12]));
     });
   });
 }
