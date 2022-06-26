d3f67b0254b73169e86513b2a2b06df5283f910e
Loc <ttloc1999@gmail.com>
2022-06-26 17:00:05 +0700

    app exit on error release mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:dice_calc/screen/calc_screen/calc_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError;
+    if (kReleaseMode) exit(1);
+  };
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('historyBox');
+
   runApp(const MyApp());
 }
 
